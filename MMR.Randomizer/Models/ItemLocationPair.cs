2bc5aeeccd1687ec8bfbf3c61570e28ae92fcf3e
isghj8 <isghj8@gmail.com>
2025-01-05 13:55:54 -0800

    Debug: add debug parameter evaluations for ItemLocations so I can read them during debugging sessions

--- MMR.Randomizer/Models/ItemLocationPair.cs
+++ MMR.Randomizer/Models/ItemLocationPair.cs
@@ -1,5 +1,8 @@
 ﻿namespace MMR.Randomizer.Models
 {
+#if DEBUG
+    [System.Diagnostics.DebuggerDisplay("item[{Item}] from loc:[{Location}]")]
+#endif
     public class ItemLocationPair
     {
         public string Item { get; set; }
